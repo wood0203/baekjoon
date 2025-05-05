93708184adccaf761d46f6897057e70b127808a3
InWoo- Lee <84387335+wood0203@users.noreply.github.com>
2025-05-05 15:09:17 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 140 ms, Memory: 19872 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Queue;
+
+public class Main {
+	
+	static HashMap<Integer, Integer> ladders = new HashMap<>();
+	static HashMap<Integer, Integer> snakes = new HashMap<>();
+	static int N, M, answer = 99;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] nm = br.readLine().split(" "); 
+		N = Integer.parseInt(nm[0]);
+		M = Integer.parseInt(nm[1]);
+		for (int i = 0; i < N; i++) {
+			String[] ab = br.readLine().split(" ");
+			ladders.put(Integer.parseInt(ab[0]), Integer.parseInt(ab[1]));
+		}
+		
+		for (int i = 0; i < M; i++) {
+			String[] ab = br.readLine().split(" ");
+			ladders.put(Integer.parseInt(ab[0]), Integer.parseInt(ab[1]));
+		}
+		
+		Queue<int []> queue = new ArrayDeque<>();
+		queue.offer(new int[] {1, 0});
+		while (!queue.isEmpty()) {
+			int[] p = queue.poll();
+			if (p[0] == 100) {
+				System.out.println(p[1]);
+				return;
+			}
+			
+			int straight = 1;
+			for (int i = 1; i <= 6; i++) {
+				if (snakes.get(p[0] + i) != null) continue;
+				
+				if (ladders.get(p[0] + i) != null) {
+					queue.offer(new int[] {ladders.get(p[0] + i), p[1] + 1});
+					continue;
+				}
+				
+				if (p[0] + i <= 100)
+					straight = Integer.max(straight, i);
+			}
+			
+			queue.offer(new int[] {straight + p[0], p[1] + 1});
+		}
+	}
+}
