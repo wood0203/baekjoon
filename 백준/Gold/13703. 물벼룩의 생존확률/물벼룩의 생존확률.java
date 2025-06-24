aaea128ed558e742fe140125b11f12e408b6988e
InWoo- Lee <84387335+wood0203@users.noreply.github.com>
2025-06-24 14:54:28 +0900

    [Gold V] Title: 물벼룩의 생존확률, Time: 68 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Gold/13703. 물벼룩의 생존확률/물벼룩의 생존확률.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+
+    static int K;
+    static long[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        K = Integer.parseInt(input[0]);
+        int N = Integer.parseInt(input[1]);
+        int col = K + N + 1;
+        dp = new long[N + 1][col];
+        dp[0][K] = 1;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < col; j++) {
+                if (j > 1) dp[i][j] += dp[i - 1][j - 1];
+                if (j < col - 1) dp[i][j] += dp[i - 1][j + 1];
+            }
+        }
+
+        long answer = 0;
+        for (int j = 1; j < col; j++) {
+            answer += dp[N][j];
+        }
+
+        System.out.println(answer);
+    }
+}
